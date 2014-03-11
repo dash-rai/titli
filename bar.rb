f14d6228c4ae0642bfcab571b303d5d94b396b9b
Darshan Rai <darshanjrai@gmail.com>
2014-03-11 17:53:50 +0530

    IRuby demo implementation using iframe

--- bar.rb
+++ bar.rb
@@ -3,22 +3,27 @@
 module Plot
   
   class Bar
-    def add_html(script)
-      header = File.read("header.html")
-      footer = File.read("footer.html")
-      puts header + script + footer #change this: return instead
+    #internal methods
+    def add_html(script, height)
+      header = File.read("/home/darshan/gsoc/code/titli/header.html")
+      footer = File.read("/home/darshan/gsoc/code/titli/footer.html")
+      result = header + script + footer
+      File.open("/home/darshan/www/titli/output.html", 'w') { |file| file.write(result) }
+      IRuby.html "<iframe src=http://localhost/titli/output.html width=500 height=#{height} style=\"border: none;\"></iframe>"
     end
   end
   
-  def Plot.bar(data_array) #define more options later. 
+  def Plot.bar(data_array) #define more options later.
     bar = Bar.new
+    scaling_factor = 1
+    height = data_array.max * scaling_factor + 20 #padding
     script = <<D3
          var dataset = #{data_array}
          var svgWidth = 750;
-         var svgHeight = 400;
+         var svgHeight = #{height};
          var barWidth = 20;
          var barSpacing = 2;
-         var scalingFactor = 10;
+         var scalingFactor = #{scaling_factor};
 
          var svg = d3.select("body").append("svg")
                      .attr("width", svgWidth)
@@ -42,7 +47,7 @@ module Plot
 
 D3
     
-    return bar.add_html(script)
+    return bar.add_html(script, height + 100)
   end
   
 end
